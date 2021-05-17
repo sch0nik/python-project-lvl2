8910280c2f20e473c87b5959afbaf26f69430272
Николай Ануфриев <sch-nikolaj@yandex.ru>
2021-05-17 08:02:34 +0300

    Исправление ошибок линтера

--- gendiff/generate_difference.py
+++ gendiff/generate_difference.py
@@ -4,8 +4,9 @@ import json
 
 import yaml
 
-from formatter_diff.plain import plain
-from formatter_diff.stylish import stylish
+from formatter_diff.json import format_json
+from formatter_diff.plain import format_plain
+from formatter_diff.stylish import format_stylish
 from gendiff import processing_diff
 
 
@@ -28,7 +29,11 @@ def compare_data(data1, data2):
                     tmp_diff = compare_data(value_data1, value_data2)
                     processing_diff.add_diff(item1, diff, tmp_diff)
                 else:
-                    processing_diff.add_update(item1, value_data1, value_data2, diff)
+                    processing_diff.add_update(
+                        item1,
+                        value_data1, value_data2,
+                        diff
+                    )
             key_list2.remove(item1)
         else:
             processing_diff.add_delete(item1, value_data1, diff)
@@ -40,44 +45,6 @@ def compare_data(data1, data2):
     return diff
 
 
-# def compare_data(data1, data2):
-#     key_data1 = list(data1.keys())
-#     key_data2 = list(data2.keys())
-#
-#     diff = processing_diff.create_diff()
-#     for item_key1 in key_data1:
-#         # получение значений из словарей
-#         # get возвращает None, если такого ключа нет
-#         value_data1 = data1.get(item_key1)
-#         value_data2 = data2.get(item_key1)
-#
-#         # возможны три варианта
-#         # такого ключа нет во втором словаре
-#         if value_data2 is None:
-#             processing_diff.add_delete(item_key1, value_data1, diff)
-#
-#         # значения в словарях по этому ключу равны
-#         elif value_data1 == value_data2:
-#             key_data2.remove(item_key1)
-#             processing_diff.add_unmodified(item_key1, value_data1, diff)
-#
-#         # вругих случаях значит что ключ есть и там и там, но значения разные
-#         else:
-#             key_data2.remove(item_key1)
-#             if type(value_data1) == dict and type(value_data2) == dict:
-#                 tmp_diff = compare_data(value_data1, value_data2)
-#                 processing_diff.add_diff(item_key1, diff, tmp_diff)
-#             else:
-#                 processing_diff.add_update(item_key1, value_data1, value_data2, diff)
-#                 # processing_diff.add_add(item_key1, value_data2, diff)
-#
-#     # дописывание остатка от второго списка
-#     for item_key2 in key_data2:
-#         processing_diff.add_add(item_key2, data2.get(item_key2), diff)
-#
-#     return diff
-#
-#
 def generate_diff(file1, file2, formatter):
     name = file1.split('.')
     name = name[-1]
@@ -92,6 +59,8 @@ def generate_diff(file1, file2, formatter):
         return 'Не поддерживаемый тип файлов'
 
     if formatter == 'stylish':
-        return stylish(compare_data(file1, file2))
+        return format_stylish(compare_data(file1, file2))
     elif formatter == 'plain':
-        return plain(compare_data(file1, file2))
+        return format_plain(compare_data(file1, file2))
+    elif formatter == 'json':
+        return format_json(compare_data(file1, file2))
